2db59f2c30f02ecde389d2933b48c1dbd3640f61
Caleb Chang <changc42@gmail.com>
2020-03-08 13:10:38 -0400

    Added search bar (fake)

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,6 +5,10 @@ export default function Landing() {
   return (
     <div className="Landing">
       <h1 style={{ fontSize: "200px" }}>Receiptify</h1>
+      <form>
+        <input type="text" placeholder="Search for an item..."></input>
+        <i class="fas fa-search"></i>
+      </form>
       <p style={{ fontSize: "50px" }}>Scan receipts, Save money</p>
     </div>
   );
