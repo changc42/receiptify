4f5afdbca2e7b68f377a7eba381773cf12592968
Caleb Chang <changc42@gmail.com>
2020-03-07 19:31:36 -0500

    configured heroku-post-build

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.get("/api/test", (req, res) => {
   res.send("in express index.js 2");
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 let PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("running on port ", PORT);
